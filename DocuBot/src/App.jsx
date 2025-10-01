de78867bfc3125a201e39b7f713de7a04d7dee0b
ABHI002684 <abhishek002684@gmail.com>
2025-10-01 23:33:12 +0530

    implemented file upload functionality

--- DocuBot/src/App.jsx
+++ DocuBot/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './components/Header'
+import FileUpload from './components/FileUpload'
+import Summary from './components/Summary'
 
 const App = () => {
+  const [uploadedFile, setUploadedFile] = useState(null);
   return (
     <div>
-      
+      <Header />
+      {
+        uploadedFile ? <Summary file={uploadedFile} /> : 
+        <FileUpload setFile={setUploadedFile} />
+      }
+     
     </div>
   )
 }
---
+++ DocuBot/src/components/FileUpload.jsx
@@ -0,0 +1,26 @@
+import {Buffer} from 'buffer'; 
+
+const FileUpload = ({setFile}) => {
+    const handleFileUpload = async(e)=>{
+        const fileUpload = await e.target.files[0].arrayBuffer();
+        const file={
+            type: e.target.files[0].type,
+            file:Buffer.from(fileUpload).toString('base64')
+        }
+        setFile(file);
+    }
+
+  return (
+    <div>
+      <h2>Get Started</h2>
+      <input 
+      type="file"
+    
+      accept='.pdf, .jpeg, .png, .jpg'
+      onChange={handleFileUpload}
+       />
+    </div>
+  )
+}
+
+export default FileUpload
