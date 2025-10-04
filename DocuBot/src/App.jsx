c34191cf16304db5524c8367e524b8852195141b
ABHI002684 <abhishek002684@gmail.com>
2025-10-04 11:43:29 +0530

    created chat component

--- DocuBot/src/App.jsx
+++ DocuBot/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Header from './components/Header'
 import FileUpload from './components/FileUpload'
 import Summary from './components/Summary'
+import Chat from './components/Chat'
 
 const App = () => {
   const [uploadedFile, setUploadedFile] = useState(null);
@@ -9,7 +10,11 @@ const App = () => {
     <div>
       <Header />
       {
-        uploadedFile ? <Summary file={uploadedFile} /> : 
+        uploadedFile ? 
+        <>
+        <Summary file={uploadedFile} />
+        <Chat file={uploadedFile} />
+        </> : 
         <FileUpload setFile={setUploadedFile} />
       }
      
---
+++ DocuBot/src/components/Chat.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { GoogleGenAI } from "@google/genai";
+import './Chat.css'
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+
+const Chat = ({file}) => {
+    
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState("");
+    
+    const handleSendMessage = async () => {
+      if(input.length){
+        let chatMessages = [...messages, {role:'user', text:input}, {role:'loader', text:''}];
+        setInput("");
+        setMessages(chatMessages);
+
+
+         try {
+            const contents = [
+                { text: `
+                  Answer this question from the context of the document:${input}
+                  Answer as a chatbot with a short message and text only (no markdown or  tag or symbol)
+                  chat history: ${JSON.stringify(messages)}
+                  ` },
+                {
+                    inlineData: {
+                        mimeType: file.type,
+                        data: file.file
+                    }
+                }
+            ];
+
+            const response = await ai.models.generateContent({
+                model: "gemini-2.5-flash",
+                contents: contents
+            });
+            
+            chatMessages= [...chatMessages.filter((msg)=>msg.role!='loader'), {role:'model', text:response.text}];
+            setMessages(chatMessages);
+        } catch (err) {
+          chatMessages= [...chatMessages.filter((msg)=>msg.role!='loader'), {role:'error', text:"error in sending message, please try again later"}];
+          setMessages(chatMessages);
+          console.log("error",err.message);
+        }
+      }
+ 
+    }
+
+  return (
+    <section className='chat-window'>
+      <h2>Chat</h2>
+      {
+        messages.length ? <div className="chat">
+            {
+
+        
+            messages.map((msg)=>(
+                <div  className={msg.role} key={msg.text} >
+                    <p>{msg.text}</p>
+                </div>
+            ))
+        }
+        </div>:''
+        }: ''
+      <div className="input-area">
+        <input type="text"
+              value={input}
+              onChange={(e)=>setInput(e.target.value)}
+               placeholder='ask any question about the document'
+        />
+         <button onClick={handleSendMessage} >send</button>
+      </div>
+    </section>
+  )
+}
+
+export default Chat
+
+
+try {
+            const contents = [
+                { text: `Summarize this document in one short paragraph
+                    (less than 100 words). use just plaintext with no markdown
+                    or HTML tags` },
+                {
+                    inlineData: {
+                        mimeType: file.type,
+                        data: file.file
+                    }
+                }
+            ];
+
+            const response = await ai.models.generateContent({
+                model: "gemini-2.5-flash",
+                contents: contents
+            });
+            setStatus("success");
+            setSummary(response.text);
+        } catch (err) {
+            setStatus("error");
+        }
